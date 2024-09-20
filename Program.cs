92cc72fe38e6299ac309e99668b810f19ee811a8
Dbaraldi <Rubensjunior8216@gmail.com>
2024-09-20 19:56:22 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿﻿Console.Clear();
+ 
+Console.Write("|"); 
+Console.Write("|    "); Console.Write("|");Console.Write("|");
+Console.WriteLine();
+ 
+Console.Write(@"\");Console.Write(@"\"); Console.Write(" ()"); Console.Write(@" /");Console.Write(@"/");
+ 
+Console.WriteLine();
+ 
+Console.Write(@"/");Console.Write(@"/");Console.Write("(__)");Console.Write(@"\");Console.Write(@"\");
+ 
+Console.WriteLine();
+ 
+Console.Write("|");
+Console.Write("|    ");Console.Write("|");Console.Write("|");
